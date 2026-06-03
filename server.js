8047f20dd13b48a438cd8122ba6934b4c2976868
xVOLKx <121677205+xVOLKx@users.noreply.github.com>
2026-06-03 15:47:10 +0300

    Исправлен server.js: убрано дублирование Pool

--- server.js
+++ server.js
@@ -9,8 +9,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-const { Pool } = require('pg');
-
 // Подключение к PostgreSQL через переменную окружения (для Render)
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
